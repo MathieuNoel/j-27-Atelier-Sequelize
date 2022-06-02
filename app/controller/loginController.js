de2f8d5e403897a0296a0481e112743623738b92
Mathieu Noel <yzorien@gmail.com>
2022-06-03 00:09:35 +0200

    avec ultra bonus de la mort (enfin presque)

--- app/controller/loginController.js
+++ app/controller/loginController.js
@@ -1,6 +1,29 @@
+const {User, Quiz} = require('../models')
+
 const loginController = {
   loginPage: (req, res) => {
     res.render('login')
+  },
+
+  loginForm: async (req, res, next) => {
+    const userConnexion = req.body
+    const connectedUser = req.session.user.find(user => user.lastname === userConnexion.lastname || user.firstname === userConnexion.firstname || user.email === userConnexion.email || user.password === userConnexion.password);
+    const checkDb = await User.findOne(
+      {where: {email: userConnexion.email}}
+    )    
+    if(!connectedUser) {
+    req.session.user.push(userConnexion);
+    }else if(connectedUser || checkDb){    
+    try {
+      const quizzes = await Quiz.findAll({include: ["author"]});       
+      res.render('index', {quizzes});      
+    } catch (error) {
+      console.log(error)
+    }  
+    } else {
+      res.send('Sorry, user already connected or not exist!')
+    }
+    
   }
 }
 
--- app/controller/signupController.js
+++ app/controller/signupController.js
@@ -1,6 +1,26 @@
+const {Quiz, User} = require('../models')
+
 const signupController= {
   signupPage: async (req, res) => {
     res.render('signup')
+  },
+
+  signupForm: async (req, res, next) => {
+    const newUser = req.body
+    const ifUserExist = req.session.user.find(user => user.lastname === newUser.lastname || user.firstname === newUser.firstname || user.email === newUser.email || user.password === newUser.password);
+    if(ifUserExist) {
+    req.session.user.push(newUser);
+    User.bulkCreate(newUser)      
+    try {
+      const quizzes = await Quiz.findAll({include: ["author"]});       
+      res.render('index', {quizzes});      
+    } catch (error) {
+      console.log(error)
+    }  
+    } else {
+      res.send('Sorry, user already exist')
+    }
+    next()
   }
 }
 
